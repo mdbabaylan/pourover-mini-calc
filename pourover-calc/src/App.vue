124d189b21f15a23aa9a26538b6e26039d10d93c
mdbabaylan <matmatmark@gmail.com>
2024-09-17 23:26:17 +0800

    added pours, and bloom todo list next

--- pourover-calc/src/App.vue
+++ pourover-calc/src/App.vue
@@ -24,8 +24,32 @@
 
       <!-- Output display -->
       <div class="content has-text-centered">
-        <p class="is-size-4 has-text-weight-semibold">Output: {{ calculatedOutput }} grams</p>
+        <p class="is-size-4 has-text-weight-semibold">Total Output: {{ calculatedOutput }} grams</p>
       </div>
+
+      <!-- Picker for selecting how many pours -->
+      <div class="field">
+        <label class="label">Select Pours</label>
+        <div class="control">
+          <div class="select is-fullwidth">
+            <select v-model="selectedRatio">
+              <option v-for="pours in pours" :key="pours" :value="pours">{{ pours }}</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="field">
+        <div class="control">
+          <input class="input" type="number" v-model="inputAmount" placeholder="Enter amount in grams" />
+        </div>
+      </div>
+
+      Add bloom amount too, then deduct bloom in total amount to divide for X number of pours (yes get better at math
+      too so u get better at coding)
+      Match the number 1,2,3,4 with First Pour, Second Pour, Third Pour, Fourth Pour
+      Use a text input, "Pour X next, new scale total should be X"
+
     </div>
   </div>
 </template>
@@ -38,6 +62,8 @@ export default {
   setup() {
     const ratios = ref([15, 16, 17]);
     const selectedRatio = ref(ratios.value[0]);
+    const pours = ref([1, 2, 3, 4]);
+    //const selectedPours = ref(pours.value[0]);
     const inputAmount = ref(0);
 
     const calculatedOutput = computed(() => {
@@ -49,6 +75,7 @@ export default {
       selectedRatio,
       inputAmount,
       calculatedOutput,
+      pours
     };
   },
 };
