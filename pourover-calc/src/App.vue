d6e6f8c40a0337db0220ca216a3dafea1badb90d
mdbabaylan <matmatmark@gmail.com>
2024-09-19 00:08:23 +0800

    v1 done. Math correct

--- pourover-calc/src/App.vue
+++ pourover-calc/src/App.vue
@@ -16,23 +16,18 @@
 
       <!-- Input for amount -->
       <div class="field">
-        <label class="label">Enter Amount (grams):</label>
+        <label class="label">Enter Coffee Amount (grams):</label>
         <div class="control">
           <input class="input" type="number" v-model="inputAmount" placeholder="Enter amount in grams" />
         </div>
       </div>
 
-      <!-- Output display -->
-      <div class="content has-text-centered">
-        <p class="is-size-4 has-text-weight-semibold">Total Output: {{ calculatedOutput }} grams</p>
-      </div>
-
       <!-- Picker for selecting how many pours -->
       <div class="field">
-        <label class="label">Select Pours</label>
+        <label class="label">Select # of pours</label>
         <div class="control">
           <div class="select is-fullwidth">
-            <select v-model="selectedRatio">
+            <select v-model="selectedPours">
               <option v-for="pours in pours" :key="pours" :value="pours">{{ pours }}</option>
             </select>
           </div>
@@ -41,15 +36,32 @@
 
       <div class="field">
         <div class="control">
-          <input class="input" type="number" v-model="inputAmount" placeholder="Enter amount in grams" />
+          <label class="label">Enter bloom amount</label>
+          <input class="input" type="number" v-model="bloomAmount" />
         </div>
       </div>
 
-      Add bloom amount too, then deduct bloom in total amount to divide for X number of pours (yes get better at math
-      too so u get better at coding)
-      Match the number 1,2,3,4 with First Pour, Second Pour, Third Pour, Fourth Pour
-      Use a text input, "Pour X next, new scale total should be X"
+      <hr class="is-divider">
 
+
+      <div v-for="(pour, index) in selectedPours" :key="index" class="field">
+        <label class="help">{{ formatPour(pour) }} pour</label>
+
+
+        <input class="input" type="text" :value="(calculatedOutput - bloomAmount) / selectedPours" readonly />
+        <label class="help">New total after {{ formatPour(pour) }} pour: {{ currentTotal(pour) }}g</label>
+
+
+
+
+      </div>
+
+    </div>
+
+    <!-- Output display -->
+    <div class="content has-text-centered">
+      <p class="is-size-4 has-text-weight-semibold">Total Output: {{ calculatedOutput }} grams</p>
+      <label class="help">(Including bloom)</label>
     </div>
   </div>
 </template>
@@ -60,14 +72,29 @@ import { ref, computed } from 'vue';
 export default {
   name: 'App',
   setup() {
-    const ratios = ref([15, 16, 17]);
-    const selectedRatio = ref(ratios.value[0]);
+    const ratios = ref([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    const selectedRatio = ref(ratios.value[5]);
     const pours = ref([1, 2, 3, 4]);
-    //const selectedPours = ref(pours.value[0]);
+    const selectedPours = ref(pours.value[2]);
     const inputAmount = ref(0);
+    const bloomAmount = ref(0);
 
     const calculatedOutput = computed(() => {
       return (inputAmount.value * selectedRatio.value).toFixed(2);
+    });
+
+    const formatPour = computed(() => {
+      return (pour) => {
+        const suffixes = ["th", "st", "nd", "rd"];
+        const value = pour % 100;
+        return value + (suffixes[(value - 20) % 10] || suffixes[value] || suffixes[0]);
+      };
+    });
+
+    const currentTotal = computed(() => {
+      return (pour) => {
+        return ((((calculatedOutput.value - bloomAmount.value) / selectedPours.value) * pour) + bloomAmount.value);
+      }
     });
 
     return {
@@ -75,7 +102,11 @@ export default {
       selectedRatio,
       inputAmount,
       calculatedOutput,
-      pours
+      formatPour,
+      pours,
+      selectedPours,
+      bloomAmount,
+      currentTotal
     };
   },
 };
